6c77e8865db199b76ef3b6bbd6a7d4b037213382
sqrl <sqrl@users.noreply.github.com>
2015-09-06 16:23:44 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Slack Logger
+Slack logger is a simple webserver that listens for messages from an outgoing Slack webhook and logs them. This can be useful for saving chat history
+from a free slack instance.
+
+The log format is one json object per line, each containing a string `user_name`, a float `timestamp` and a string `message.` Example:
+    {"user_name":"blackpatch","timestamp":"1441578779.017708","text":"How To Dismantle An Atomic Bomb, like in that U2 album, Achtung Baby"}
+
+The logs are rotated every 100 MB using `lumberjack` ([https://github.com/natefinch/lumberjack]).
+
+## Installation
+ * Crreate an outgoing webhook integration in slack and note the secret token.
+ * Copy `config.toml.example` to `config.toml`.
+ * Fill in the fields of `config.toml` with your secret token, the location where you want to save your logs, and the port you want slack-logger to listen on.
+ * `go run slack-logger.go`
+ * In the outgoing webhook integration, fill in the url for the machine for your slack-logger. Remember to include the port.
+
+## Known Issues
+ * By design slack-logger only logs user message events.
+ * slack-logger uses usernames rather than ids, so name changes would need to be tracked separately if real identity is important.
+ * The connection is not SSL yet. [http://golang.org/src/crypto/tls/generate_cert.go] could be used for this.
+ * There should be more logging around attempted connections.
---
+++ slack-logger.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "github.com/pelletier/go-toml"
+    "gopkg.in/natefinch/lumberjack.v2"
+    "log"
+    "net/http"
+    "os"
+    "path"
+)
+
+// Represents entries in our log files.
+type line struct {
+    UserName string `json:"user_name"`
+    Timestamp string `json:"timestamp"`
+    Text string `json:"text"`
+}
+
+// Globals needed by the handler.
+var secretToken string
+var logDirectory string
+
+// The web handler which receives messages and logs them.
+func handler(w http.ResponseWriter, r *http.Request) {
+    // Reject requests that don't have the shared secret with slack.
+    if (r.Method != "POST" || r.FormValue("token") != secretToken) {
+        w.WriteHeader(http.StatusForbidden)
+        fmt.Fprintf(os.Stderr, "Bad request from %s\n", r.RemoteAddr, r)
+        return
+    }
+
+    lineJson := &line{
+        UserName: r.FormValue("user_name"),
+        Timestamp: r.FormValue("timestamp"),
+        Text: r.FormValue("text")}
+    bytes, err := json.Marshal(lineJson)
+    if err != nil {
+        fmt.Fprintln(os.Stderr, "Error marshalling for request:\n", r)
+        return
+    }
+
+    log.Println(bytes)
+}
+
+
+func main() {
+    // Read config.toml
+    config, err := toml.LoadFile("config.toml")
+    if err != nil {
+        fmt.Fprintln(os.Stderr, "Error opening config file. Did you remember to `cp config.toml.example config.toml`?\n", err.Error())
+        os.Exit(1)
+    }
+    secretToken = config.Get("slack-logger.secret_token").(string)
+    logDirectory = config.Get("slack-logger.log_directory").(string)
+    port := config.Get("slack-logger.port").(int64)
+
+    // Set up our logger.
+    log.SetOutput(&lumberjack.Logger{ Filename: path.Join(logDirectory, "slacklog.txt") })
+    log.SetPrefix("")
+    log.SetFlags(0)
+
+    // Begin listening.
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
